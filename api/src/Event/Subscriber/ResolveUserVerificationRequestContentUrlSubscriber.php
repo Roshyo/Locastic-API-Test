6be805aacf06e75877ea1057a80b7840e3a619b1
Roshyo <stefdecock68@gmail.com>
2019-10-20 19:05:04 +0200

    WIP Uploading file

---
+++ api/src/Event/Subscriber/ResolveUserVerificationRequestContentUrlSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Event\Subscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use ApiPlatform\Core\Util\RequestAttributesExtractor;
+use App\Entity\UserVerificationRequest;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Vich\UploaderBundle\Storage\StorageInterface;
+
+final class ResolveUserVerificationRequestContentUrlSubscriber implements EventSubscriberInterface
+{
+    private $storage;
+
+    public function __construct(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::VIEW => ['onPreSerialize', EventPriorities::PRE_SERIALIZE],];
+    }
+
+    public function onPreSerialize(ViewEvent $event): void
+    {
+        $controllerResult = $event->getControllerResult();
+        $request = $event->getRequest();
+
+        if ($controllerResult instanceof Response || !$request->attributes->getBoolean('_api_respond', true)) {
+            return;
+        }
+
+        if (!($attributes = RequestAttributesExtractor::extractAttributes($request)) || !\is_a($attributes['resource_class'], UserVerificationRequest::class, true)) {
+            return;
+        }
+
+        $userVerificationRequests = $controllerResult;
+
+        if (!\is_iterable($userVerificationRequests)) {
+            $userVerificationRequests = [$userVerificationRequests];
+        }
+
+        foreach ($userVerificationRequests as $userVerificationRequest) {
+            if (!$userVerificationRequest instanceof UserVerificationRequest) {
+                continue;
+            }
+
+            $userVerificationRequest->setIDImagePath($this->storage->resolveUri($userVerificationRequest, 'IDImage'));
+        }
+    }
+}
